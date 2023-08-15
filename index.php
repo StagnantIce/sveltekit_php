b64b326758dd486edd85a5856494a2d7edea3b4d
stag <stag@wisebits.com>
2023-08-15 20:22:03 +0300

    Fix

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 $url = $_SERVER['REQUEST_URI'];
 $path = parse_url($url, PHP_URL_PATH);
 $pathWithoutSlash = substr($path, 1) ?: 'index';
-echo require_once(__DIR__ . "/build/$pathWithoutSlash.html");
+require_once(__DIR__ . "/build/$pathWithoutSlash.html");
